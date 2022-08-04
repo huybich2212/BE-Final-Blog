f6a2bdf270a16e5e1674214b380cbcfe08dca438
kenproxx <kienhue98@gmail.com>
2022-08-04 09:57:32 +0700

    list user

--- src/main/java/com/example/blog/controller/GuestController.java
+++ src/main/java/com/example/blog/controller/GuestController.java
@@ -1,7 +1,9 @@
 package com.example.blog.controller;
 
 import com.example.blog.model.Post;
+import com.example.blog.model.User;
 import com.example.blog.service.impl.PostServiceImpl;
+import com.example.blog.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,9 @@ import java.util.Optional;
 public class GuestController {
     @Autowired
     private PostServiceImpl postService;
+
+    @Autowired
+    private UserServiceImpl userService;
 
     // find all post by status public
     @GetMapping("")
@@ -36,5 +41,11 @@ public class GuestController {
     public ResponseEntity<Iterable<Post>>findAllByTitle(@RequestParam String title) {
         Iterable<Post> posts = postService.findByTitleContaining(title);
         return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<Iterable<User>> findAllUser() {
+        Iterable<User> users = userService.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
